c70d232647867e74e1928a3a9863d36c40e29400
Joao Meneses <joao.meneses.97.97@gmail.com>
2019-09-16 17:32:42 +0100

    Modo de jogo contra um bot (Odisseia)

--- RACE_odisseia/RACE_odisseia.ino
+++ RACE_odisseia/RACE_odisseia.ino
@@ -18,11 +18,16 @@
 #define ACEL 0.6
 #define DRAG 0.04
 
+//Ghost
+#define GBASESPD 1.4
+#define GSPDINC 0.1
+
 //Player colors
 #define C_PLAYER1 CRGB(0, 255, 0)
 #define C_PLAYER2 CRGB(255, 0, 0)
 #define C_PLAYER3 CRGB(0, 0, 255)
 #define C_PLAYER4 CRGB(255, 255, 0)
+#define C_PLAYERG CRGB(255,255,255)
 
 #define BEEP_FREQUENCY 700
 #define BEEP_DURATION 3
@@ -49,7 +54,7 @@ int tempo[] = {
   12, 12, 12, 12,
   12, 12, 12, 12,
 };
- 
+
 void buzz(int targetPin, long frequency, long length) {
   long delayValue = 1000000 / frequency / 2; // calculate the delay value between transitions
   //// 1 second's worth of microseconds, divided by the frequency, then split in half since
@@ -69,15 +74,22 @@ float p1_pos;
 float p2_pos;
 float p3_pos;
 float p4_pos;
+float pg_pos;
 float p1_speed;
 float p2_speed;
 float p3_speed;
 float p4_speed;
+float pg_speed;
 
 bool p1_btn_last = false;
 bool p2_btn_last = false;
 bool p3_btn_last = false;
 bool p4_btn_last = false;
+
+bool p1_finish = false;
+bool p2_finish = false;
+bool p3_finish = false;
+bool p4_finish = false;
 
 short beeping = 0;
 
@@ -102,9 +114,15 @@ void drawPlayer4() {
   }
 }
 
+void drawPlayerG() {
+  for (unsigned short i = 0; i <= pg_pos / NUM_LEDS; i++) {
+    leds[((unsigned int)pg_pos - i) % NUM_LEDS] = C_PLAYERG;
+  }
+}
 void drawPlayers() {
   //change draw order in order to see players in same pos
   short currentMills = millis() % 1000; // 0 - 999
+  drawPlayerG();
   if (currentMills < 250) {
     drawPlayer1();
     drawPlayer2();
@@ -162,27 +180,40 @@ void applyDrag() {
 }
 
 void calculatePositions() {
+  int max_dist = NUM_LEDS * NUM_LAPS;
   int temp = (int)p1_pos % NUM_LEDS;
   p1_pos += p1_speed;
-  if ((int)p1_pos%NUM_LEDS < temp)
+  if ((int)p1_pos % NUM_LEDS < temp)
     beep();
+  if (p1_pos > max_dist)
+    p1_finish = true;
   temp = (int)p2_pos % NUM_LEDS;
   p2_pos += p2_speed;
-  if ((int)p2_pos%NUM_LEDS < temp)
+  if ((int)p2_pos % NUM_LEDS < temp)
     beep();
+  if (p2_pos > max_dist)
+    p2_finish = true;
   temp = (int)p3_pos % NUM_LEDS;
   p3_pos += p3_speed;
-  if ((int)p3_pos%NUM_LEDS < temp)
+  if ((int)p3_pos % NUM_LEDS < temp)
     beep();
+  if (p3_pos > max_dist)
+    p3_finish = true;
   temp = (int)p4_pos % NUM_LEDS;
   p4_pos += p4_speed;
-  if ((int)p4_pos%NUM_LEDS < temp)
+  if ((int)p4_pos % NUM_LEDS < temp)
+    beep();
+  if (p4_pos > max_dist)
+    p4_finish = true;
+  temp = (int)pg_pos % NUM_LEDS;
+  pg_pos += pg_speed;
+  if ((int)pg_pos % NUM_LEDS < temp)
     beep();
 }
 
 bool inGame() {
   int max_dist = NUM_LAPS * NUM_LEDS;
-  if (p1_pos >= max_dist || p2_pos >= max_dist || p3_pos >= max_dist || p4_pos >= max_dist) {
+  if (pg_pos >= max_dist) {
     return false;
   }
   return true;
@@ -193,7 +224,7 @@ void beep() {
   beeping = BEEP_DURATION;
 }
 
-void startRace() {
+void startRace(float spd) {
   p1_pos = 0;
   p1_speed = 0;
   p1_btn_last = true;
@@ -206,7 +237,14 @@ void startRace() {
   p4_pos = 0;
   p4_speed = 0;
   p4_btn_last = true;
+  pg_pos = 0;
+  pg_speed = spd;
   beeping = 0;
+
+  p1_finish = false;
+  p2_finish = false;
+  p3_finish = false;
+  p4_finish = false;
 
   FastLED.clear();
   leds[7] = CRGB::Red;
@@ -214,7 +252,7 @@ void startRace() {
   leds[9] = CRGB::Red;
   FastLED.show();
   delay(500);
-  
+
   tone(PIN_BUZZER, 440, 500);
   delay(750);
 
@@ -237,66 +275,85 @@ void startRace() {
 }
 
 void winnerSound() {
- int size = sizeof(melody) / sizeof(int);
-    for (int thisNote = 0; thisNote < size; thisNote++) {
-      int noteDuration = 1000 / tempo[thisNote];
- 
-      buzz(PIN_BUZZER, melody[thisNote], noteDuration);
-      //30% to separate the notes
-      int pauseBetweenNotes = noteDuration * 1.30;
-      delay(pauseBetweenNotes);
- 
-      // stop the tone playing aka noTone:
-      buzz(PIN_BUZZER, 0, noteDuration);
-    }
+  int size = sizeof(melody) / sizeof(int);
+  for (int thisNote = 0; thisNote < size; thisNote++) {
+    int noteDuration = 1000 / tempo[thisNote];
+
+    buzz(PIN_BUZZER, melody[thisNote], noteDuration);
+    //30% to separate the notes
+    int pauseBetweenNotes = noteDuration * 1.30;
+    delay(pauseBetweenNotes);
+
+    // stop the tone playing aka noTone:
+    buzz(PIN_BUZZER, 0, noteDuration);
+  }
 }
 
 void showWinner() {
-  unsigned int max_dist = NUM_LAPS * NUM_LEDS;
-  if (p1_pos >= max_dist) {
-    FastLED.clear();
-    for (unsigned int i = 0; i < NUM_LEDS/15; i++) {
-      leds[i] = C_PLAYER1;
-    }
-    FastLED.show();
-    winnerSound();
+  FastLED.clear();
+  int num = 0;
+  if (p1_finish) {
+    leds[0] = C_PLAYER1;
+    leds[1] = C_PLAYER1;
+    leds[2] = C_PLAYER1;
+    num++;
   }
-  if (p2_pos >= max_dist) {
-    FastLED.clear();
-    for (unsigned int i = 0; i < NUM_LEDS/15; i++) {
-      leds[i] = C_PLAYER2;
-    }
-    FastLED.show();
-    winnerSound();
+
+  if (p2_finish) {
+    leds[3] = C_PLAYER2;
+    leds[4] = C_PLAYER2;
+    leds[5] = C_PLAYER2;
+    num++;
   }
-  if (p3_pos >= max_dist) {
-    FastLED.clear();
-    for (unsigned int i = 0; i < NUM_LEDS/15; i++) {
-      leds[i] = C_PLAYER3;
-    }
-    FastLED.show();
-    winnerSound();
+
+  if (p3_finish) {
+    leds[6] = C_PLAYER3;
+    leds[7] = C_PLAYER3;
+    leds[8] = C_PLAYER3;
+    num++;
   }
-  if (p4_pos >= max_dist) {
-    FastLED.clear();
-    for (unsigned int i = 0; i < NUM_LEDS/15; i++) {
-      leds[i] = C_PLAYER4;
-    }
-    FastLED.show();
-    winnerSound();
+
+  if (p4_finish) {
+    leds[9] = C_PLAYER4;
+    leds[10] = C_PLAYER4;
+    leds[11] = C_PLAYER4;
+    num++;
   }
+
+
+  if (!p1_finish && !p2_finish && !p3_finish && !p4_finish) {
+    for (unsigned int i = 0; i < NUM_LEDS / 7; i++) {
+      leds[i] = C_PLAYERG;
+    }
+  }
+  FastLED.show();
+  Serial.print(num);
+  Serial.println(" jogadores venceram o fantasma!");
+  winnerSound();
+
 }
 
-void standby(){
+float standby() {
   //wait for all buttons to be pressed to reset the startRace
   int pos = 0;
-  while (digitalRead(PIN_PLAYER1) && digitalRead(PIN_PLAYER2) && digitalRead(PIN_PLAYER3) && digitalRead(PIN_PLAYER4)) {
+  int dificulty = -1;
+  Serial.println("Enviar a dificuldade de 0-5: ");
+  while (Serial.available()) {
+    Serial.read();
+  }
+  while (true) {
     delay(100);
     FastLED.clear();
     leds[pos++ % NUM_LEDS] = CRGB(millis() % 256, micros() % 256, (pos * analogRead(1)) % 256);
     FastLED.show();
+    if (Serial.available()) {
+      dificulty = Serial.parseInt();
+      if (dificulty >= 0 && dificulty < 6)
+        return GBASESPD + (GSPDINC * dificulty);
+    }
   }
   FastLED.clear();
+  return GBASESPD + (GSPDINC * dificulty);
 }
 
 void setup() {
@@ -306,11 +363,13 @@ void setup() {
   pinMode(PIN_PLAYER3, INPUT_PULLUP);
   pinMode(PIN_PLAYER4, INPUT_PULLUP);
   pinMode(PIN_BUZZER, OUTPUT);
+  Serial.begin(57600);
 }
 
 void loop() {
-  standby();
-  startRace();
+  float spd;
+  spd = standby();
+  startRace(spd);
   while (inGame()) {
     checkButtons();
     applyDrag();
@@ -318,7 +377,7 @@ void loop() {
     FastLED.clear();
     drawPlayers();
     if (beeping == 0) {
-        noTone(PIN_BUZZER);
+      noTone(PIN_BUZZER);
     }
     if (beeping > 0) {
       beeping--;
